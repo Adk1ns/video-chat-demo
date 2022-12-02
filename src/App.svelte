83b48a997386b86f1ba4c1202c3b7ca83f4d5d45
Jered Adkins <jeredkins@gmail.com>
2022-12-01 17:10:11 -0800

    clean up

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
   import {Peer} from 'peerjs'
-let peer = new Peer();
-let codeid = ""
-let videocurrent;
-let videoEl;
-let youid = ""
+  let peer = new Peer();
+  let codeid = ""
+  let videocurrent;
+  let videoEl;
+  let youid = ""
 
   // get your ID
   peer.on("open",(id)=>{
@@ -45,24 +45,15 @@ let renderYouwebcam = (stream)=>{
   videoEl.srcObject = stream
   videoEl.play()
 }
-
-</script>
-<div>
-  you id cam  = {youid}
-  <br>
-  code : <input type=""
-  bind:value={codeid} name="">
-
-  <button
-  on:click={async()=>{
-    var conn = peer.connect(codeid)
+  const handleClick = async()=>{
+    let conn = peer.connect(codeid)
     conn.on("data",(data)=>{
       console.log("new data " + data)
     })
     conn.on("open",function(){
       conn.send("hi")
     })
-    // OPEN YOU WEBAM
+    // open webcam
     await navigator.mediaDevices.getUserMedia({
       video:true,
       audio:true
@@ -72,28 +63,33 @@ let renderYouwebcam = (stream)=>{
       videocurrent.play()
       call.on("stream",renderYouwebcam)
     }).catch(err=>console.log("have error " + err))
-  }}
-  >
-  connect</button>
+  }
+</script>
 
-  <!--FRIEND -->
+<div>
+  you id cam  = {youid}
+  <br>
+  code : <input type="text" bind:value={codeid} name="">
+
+  <button on:click={handleClick}>connect</button>
+
   <div>
     <p>friend</p>
-  <video 
-  bind:this={videoEl}
-  width="400" height="400" autoplay={true}>
-    <track kind="captions" src="">
-  </video>
+    <video 
+      bind:this={videoEl}
+      width="400" height="400" autoplay={true}>
+      <track kind="captions" src="">
+    </video>
   </div>
+
   <br>
 
-  <!-- YOU -->
   <div>
     <p>me</p>
-  <video 
-  bind:this={videocurrent}
-  width="400" height="400" autoplay={true}>
-    <track kind="captions" src="">
-  </video>
+    <video 
+      bind:this={videocurrent}
+      width="400" height="400" autoplay={true}>
+      <track kind="captions" src="">
+    </video>
   </div>
 </div>
